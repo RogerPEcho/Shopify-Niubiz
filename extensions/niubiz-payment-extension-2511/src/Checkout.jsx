063c24eea126d4bc7716a435d55d7ade2163c879
EL3M3R <veraraul495@gmail.com>
2023-11-25 18:09:41 -0500

    add config

---
+++ extensions/niubiz-payment-extension-2511/src/Checkout.jsx
@@ -0,0 +1,22 @@
+import {
+  Banner,
+  useApi,
+  useTranslate,
+  reactExtension,
+} from '@shopify/ui-extensions-react/checkout';
+
+export default reactExtension(
+  'purchase.checkout.block.render',
+  () => <Extension />,
+);
+
+function Extension() {
+  const translate = useTranslate();
+  const { extension } = useApi();
+
+  return (
+    <Banner title="niubiz-payment-extension-2511">
+      {translate('welcome', {target: extension.target})}
+    </Banner>
+  );
+}
